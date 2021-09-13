fea195d137885309b1f463247986e961801ff556
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-09-13 19:28:49 -0400

    made a colourful error message

---
+++ assertEqual.js
@@ -0,0 +1,21 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+// Comparing identical strings
+assertEqual("Hello Isabel", "Hello Isabel");
+// Comparing non-identical strings
+assertEqual("Good Luck", "okay");
+// Comparing identical numbers
+assertEqual(155, 155);
+// Comparing non-identical numbers
+assertEqual(1, 1000);
