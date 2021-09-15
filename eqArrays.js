b60d3f0114f3a9c2e4fc5cfd7d5c7340642c3bb7
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-09-15 13:00:18 -0400

    check equality of two Arrays

---
+++ eqArrays.js
@@ -0,0 +1,41 @@
+// ASSERT EQUAL FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//EQARRAYS FUNCTION IMPLEMENTATION 
+
+const eqArrays = function(arrayOne, arrayTwo){
+
+  let result = true; 
+  if(arrayOne.length === arrayTwo.length) {
+    for(let i = 0; i < arrayOne.length; i++) {
+      if(arrayOne[i] !== arrayTwo[i]) {
+        result = false;
+      } else {
+        result = true;
+      }
+    }
+  } else {
+    result = false;
+  }
+  return result;
+};
+
+//TEST CASES
+
+//eqArrays([1, 2, 3], [1, 2, 3]) // => true
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+//eqArrays([1, 2, 3], [3, 2, 1]) // => false
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+//eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+
+//eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
