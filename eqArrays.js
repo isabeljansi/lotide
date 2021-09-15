7ee6b2edc7b48a5495657bf9f37645d58f022120
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-09-15 17:35:39 -0400

    updated code

--- eqArrays.js
+++ eqArrays.js
@@ -12,16 +12,13 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(arrayOne, arrayTwo){
 
   let result = true; 
-  if(arrayOne.length === arrayTwo.length) {
-    for(let i = 0; i < arrayOne.length; i++) {
-      if(arrayOne[i] !== arrayTwo[i]) {
-        result = false;
-      } else {
-        result = true;
-      }
-    }
-  } else {
+  if(arrayOne.length !== arrayTwo.length) {
     result = false;
+  }
+  for(let i = 0; i < arrayOne.length; i++) {
+    if(arrayOne[i] !== arrayTwo[i]) {
+        result = false;
+    } 
   }
   return result;
 };
@@ -29,7 +26,7 @@ const eqArrays = function(arrayOne, arrayTwo){
 //TEST CASES
 
 //eqArrays([1, 2, 3], [1, 2, 3]) // => true
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 3, 3, ], [1, 2, 3]), true);
 
 //eqArrays([1, 2, 3], [3, 2, 1]) // => false
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
