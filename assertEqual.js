18ade38f057e3d487115a93a517fdfacd431b5b2
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-09-20 19:30:12 -0400

    refartored to export

--- assertEqual.js
+++ assertEqual.js
@@ -1,21 +1,12 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅ Passed: ${actual} === ${expected}`);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`❌ Failed: ${actual} !== ${expected}`);
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 
-// Comparing identical strings
-assertEqual("Hello Isabel", "Hello Isabel");
-// Comparing non-identical strings
-assertEqual("Good Luck", "okay");
-// Comparing identical numbers
-assertEqual(155, 155);
-// Comparing non-identical numbers
-assertEqual(1, 1000);
+
+module.exports = assertEqual;
