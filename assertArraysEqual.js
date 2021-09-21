ffbfcbe706b1a41cd303b7f44584b3c9cb0855d8
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-09-20 20:55:56 -0400

    refactor to export module

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,10 @@
-const eqArrays = function(arrayOne, arrayTwo){
-  let result = true; 
-  if(arrayOne.length !== arrayTwo.length) {
-    result = false;
-  } else {
-    for(let i = 0; i < arrayOne.length; i++) {
-      if(arrayOne[i] !== arrayTwo[i]) {
-        result = false;
-      } 
-    }
-  }
-  return result;
-};
+
 
 const assertArraysEqual = function (actualArray,expectedArray){
   let finalResult = eqArrays(actualArray,expectedArray);
   return (finalResult ? console.log(`✅ Passed ${actualArray} === ${expectedArray}`) : console.log(`❌ Failed ${actualArray} !== ${expectedArray}`))
 }; 
 
-assertArraysEqual([1,2,3,4],[1,2,3]); 
-assertArraysEqual([1,2,3,],[1,2,3]); 
-assertArraysEqual([1,3,3,],[1,2,3]);
+
+
+module.exports = assertArraysEqual;
